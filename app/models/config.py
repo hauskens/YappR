a8156ce2fc792265dae7feb9a02d767115fe52e9
hauskens <hauskens-git@disp.lease>
2025-04-06 23:14:43 +0200

    add docker build and restructure project folder

--- app/models/config.py
+++ app/models/config.py
@@ -7,10 +7,12 @@ class Config:
         self.app_secret: str = os.environ.get("APP_SECRET", "omgtesties")
         self.database_uri: str = os.environ.get(
             "DB_URI",
-            "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres",
+            "postgresql+psycopg://postgres:mysecretpassword@localhost:5432/postgres",
         )
         self.log_level: str | int = os.environ.get("LOG_LEVEL", logging.DEBUG)
         self.storage_location: str = os.environ.get(
             "STORAGE_LOCATION", "./test_storage"
         )
         self.redis_uri: str = os.environ.get("REDIS_URI", "redis://localhost:6379/0")
+        self.app_port: str = os.environ.get("PORT", "5000")
+        self.app_host: str = os.environ.get("HOST", "0.0.0.0")

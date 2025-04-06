6eb74a417a76e613ba02b77618beb04c7c22a259
hauskens <hauskens-git@disp.lease>
2025-04-06 23:14:37 +0200

    add some base classes in python

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from vodmeta!")
+import enum
+from sqlalchemy import ForeignKey
+from sqlalchemy import String, Integer, Enum
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.schema import CreateTable
 
 
-if __name__ == "__main__":
-    main()
+class Base(DeclarativeBase):
+    pass
+
+
+class Broadcaster(Base):
+    __tablename__: str = "broadcaster"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250))
+
+
+class Platforms(Base):
+    __tablename__: str = "platforms"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250))
+    url: Mapped[str] = mapped_column(String(1000))
+
+
+class VideoType(enum.Enum):
+    unknown = 1
+    vod = 2
+    clip = 3
+    edit = 4
+
+
+class Channels(Base):
+    __tablename__: str = "channels"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250))
+    broadcaster_id: Mapped[str] = mapped_column(ForeignKey("broadcaster.id"))
+    broadcaster: Mapped["Broadcaster"] = relationship()
+    platform_id: Mapped[int] = mapped_column(ForeignKey("platforms.id"))
+    platform: Mapped["Platforms"] = relationship()
+    main_video_type: Mapped[int] = mapped_column(Enum(VideoType))
+
+
+print(CreateTable(Channels.__table__).compile(dialect=postgresql.dialect()))

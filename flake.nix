e3c7cb39106479fac2bd8c90c1653da7577d1c51
hauskens <hauskens-git@disp.lease>
2025-04-06 23:14:45 +0200

    nix wip

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
       packages = forAllSystems (system:
-        import ./packages { pkgs = nixpkgs.legacyPackages.${system}; });
+        import ./default.nix { pkgs = nixpkgs.legacyPackages.${system}; });
 
       formatter =
         forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);

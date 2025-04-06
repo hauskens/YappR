11161f2d16aa40e5dde2a8a7e009fe5a46f6ac52
hauskens <hauskens-git@disp.lease>
2025-04-06 23:14:38 +0200

    add video class

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import enum
-from sqlalchemy import ForeignKey
-from sqlalchemy import String, Integer, Enum
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, String, Integer, Enum, create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.schema import CreateTable
 
@@ -16,21 +12,21 @@ class Base(DeclarativeBase):
 class Broadcaster(Base):
     __tablename__: str = "broadcaster"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(250))
+    name: Mapped[str] = mapped_column(String(250), unique=True)
 
 
 class Platforms(Base):
     __tablename__: str = "platforms"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(250))
+    name: Mapped[str] = mapped_column(String(250), unique=True)
     url: Mapped[str] = mapped_column(String(1000))
 
 
 class VideoType(enum.Enum):
-    unknown = 1
-    vod = 2
-    clip = 3
-    edit = 4
+    Unknown = "unknown"
+    VOD = "vod"
+    Clip = "clip"
+    Edit = "edit"
 
 
 class Channels(Base):
@@ -41,7 +37,18 @@ class Channels(Base):
     broadcaster: Mapped["Broadcaster"] = relationship()
     platform_id: Mapped[int] = mapped_column(ForeignKey("platforms.id"))
     platform: Mapped["Platforms"] = relationship()
-    main_video_type: Mapped[int] = mapped_column(Enum(VideoType))
+    main_video_type: Mapped[str] = mapped_column(
+        Enum(VideoType), default=VideoType.Unknown
+    )
 
 
-print(CreateTable(Channels.__table__).compile(dialect=postgresql.dialect()))
+class Video(Base):
+    __tablename__: str = "video"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250))
+    video_type: Mapped[str] = mapped_column(Enum(VideoType))
+    channel_id: Mapped[int] = mapped_column(ForeignKey("channels.id"))
+    channel: Mapped["Channels"] = relationship()
+    platform_ref: Mapped[str] = mapped_column(String(), unique=True)
+
+

c53b1c640a1dc10ce82b58088515b179be1baba7
hauskens <hauskens-git@disp.lease>
2025-04-06 23:14:38 +0200

    flask wip

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import enum
-from sqlalchemy import ForeignKey, String, Integer, Enum, create_engine, select
+from sqlalchemy import Engine, ForeignKey, String, Integer, Enum, create_engine, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.schema import CreateTable
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Base(DeclarativeBase):
@@ -52,9 +54,64 @@ class Video(Base):
     platform_ref: Mapped[str] = mapped_column(String(), unique=True)
 
 
-engine = create_engine("sqlite:///testdb.sqlite", echo=True)
+# def init() -> Engine:
+#     engine = create_engine("sqlite:///testdb.sqlite", echo=True)
+#     Base.metadata.create_all(engine)
+#     return engine
 
-Base.metadata.create_all(engine)
+
+db = SQLAlchemy(model_class=Base)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/broadcasters")
+def broadcaster_list():
+    broadcasters = db.session.execute(
+        select(Broadcaster).order_by(Broadcaster.name)
+    ).first()
+    # return f"{broadcasters.}"
+    return "uhh"
+
+
+@app.route("/broadcaster/<name>")
+def check(name: str):
+    # return "Flask is working"
+    # broadcasters = db.session.execute(db.select(Broadcaster)).scalars()
+    broadcaster = db.get_or_404(Broadcaster, name)
+
+    return broadcaster
+
+
+@app.route("/create/<name>")
+def create_broadcaster(name: str):
+    db.session.add(Broadcaster(name=name))
+    db.session.commit()
+    return name
+
+
 # engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
+# with Session(engine) as session:
+#     testBroadcaster = Broadcaster(name="testies")
+#     session.add(testBroadcaster)
+#     session.commit()
+
+# session = Session(engine)
+# test = select(Broadcaster).where(Broadcaster.name.in_(["testies"]))
+#
+# bc = session.scalars(test).one_or_none()
+# if bc:
+#     bc.name = "buhh"
+#     # session.commit()
+#
+# test2 = select(Broadcaster)
+# for t in session.scalars(test2):
+#     print(f"hello {t.name}")
+
 # print(CreateTable(Video.__table__).compile(dialect=postgresql.dialect()))
+if __name__ == "__main__":
+    app.run()

92cdb5f892ef4ca71088f2c753500b16f6ce0e81
hauskens <hauskens-git@disp.lease>
2025-06-24 20:07:49 +0200

    init working pytest setup

--- app/rate_limit.py
+++ app/rate_limit.py
@@ -1,13 +1,11 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from app.models.config import config
 from flask_login import current_user # type: ignore
 
 # Configure rate limiter
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["2000 per day", "200 per hour"],
-    storage_uri=config.redis_uri
 )
 
 # Custom rate limit exemption for authenticated users

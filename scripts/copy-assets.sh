059b6ab48e93f0108233f6a4ceb496a5a18d6f28
hauskens <hauskens-git@disp.lease>
2025-06-28 16:05:11 +0200

    better hosting of github-buttons

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -9,14 +9,10 @@ cp node_modules/htmx.org/dist/htmx.min.js app/static/js/
 cp node_modules/socket.io-client/dist/socket.io.min.js app/static/js/
 cp node_modules/chart.js/dist/chart.umd.js app/static/js/
 cp node_modules/chartjs-plugin-zoom/dist/chartjs-plugin-zoom.min.js app/static/js/
+cp node_modules/github-buttons/dist/buttons.js app/static/js/
 
 echo "✓ Copied all JavaScript dependencies"
 
 cp node_modules/bootstrap-icons/font/bootstrap-icons.css app/static/css/
 cp -r node_modules/bootstrap-icons/font/fonts/* app/static/css/fonts/ 
 echo "✓ Copied Bootstrap Icons"
-
-# Download external assets that aren't on npm
-echo "Downloading external assets..."
-
-curl -s https://buttons.github.io/buttons.js -o app/static/js/buttons.js
